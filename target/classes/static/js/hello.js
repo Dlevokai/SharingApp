36b33f798ed9a8800714100efa2fa491b7d5906c
dan Iakovel <dan Iakovel>
2017-01-16 13:18:51 +0000

    added ng-routing, repushing correct files (intellij needs target folder files, not resources)

--- target/classes/static/js/hello.js
+++ target/classes/static/js/hello.js
@@ -1,32 +1,39 @@
-var app = angular.module('hello', []);
+var app = angular.module('hello', ["ngRoute"]);
 
     app.controller('home', function($scope) {
-	  
-	  $scope.salutation = {id: 'yyy', content: 'good bye!' } 
+
+	  $scope.salutation = {id: 'yyy', content: 'good bye!' }
       $scope.greeting = {id: 'xxx', content: 'hard coded!'}
     })
-    
+
     app.controller('httpTest', function($scope, $http) {
 	$http.get('http://rest-service.guides.spring.io/greeting').
 	    then(function(response) {
 	        $scope.greeting = response.data;
 	    });
     });
-    
+
     app.controller('JavaHttpTest', function($scope, $http) {
     	$http.get('./angularJsCall').
     	    then(function(response) {
     	    	console.log(response.data);
     	        $scope.greeting = response.data;
     	    });
-    }); 
-    
+    });
+
     app.controller('JavaHttpGetBal', function($scope, $http) {
     	$http.get('./getBankData').
     	    then(function(response) {
     	    	console.log(response.data);
     	        $scope.bankData = response.data;
     	    });
-    }); 
+    });
+
+    app.config(function($routeProvider) {
+    	  $routeProvider
+    	  .when("/graph", {
+    	    templateUrl : "/graph.html"
+    	  });
+    });
     
   
