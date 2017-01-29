4db76b1d434f05dfd4f15235106d453a51be6f2b
Your Name <D.iakovel@gmail.com>
2017-01-29 17:12:50 -0500

    code cleanup

--- target/classes/static/js/hello.js
+++ target/classes/static/js/hello.js
@@ -1,38 +1,5 @@
 var app = angular.module('hello', [ "ngRoute" ]);
 
-app.controller('home', function($scope) {
-	$scope.salutation = {
-		id : 'yyy',
-		content : 'good bye!'
-	}
-	$scope.greeting = {
-		id : 'xxx',
-		content : 'hard coded!'
-	}
-});
-
-app.controller('httpTest', function($scope, $http) {
-	$http.get('http://rest-service.guides.spring.io/greeting').then(
-			function(response) {
-				$scope.greeting = response.data;
-			});
-});
-
-app.controller('JavaHttpTest', function($scope, $http) {
-	$http.get('./angularJsCall').then(function(response) {
-		console.log(response.data);
-		$scope.greeting = response.data;
-	});
-});
-
-app.controller('H2DataTest', function($scope, $http) {
-	$http.get('./getH2Data').then(function(response) {
-		/*console.log(response.data);*/
-		$scope.product = response.data;
-	});
-});
-
-
 app.controller('JavaHttpGetBal', function($scope, $http) {
 	$http.get('./getBankData').then(function(response) {
 		console.log(response.data);
@@ -66,12 +33,3 @@ app.config(function($routeProvider) {
       		templateUrl : "/login.html"
     });
 });
-
-/*app.controller('navBarCtrl', function($scope) {
-	$scope.showHeader = true;
-});*/
-
-/*app.controller('routeController', ['$scope', '$location', function($scope, $location) {
-	$scope.showHeader = true;
-  }]);*/
-
